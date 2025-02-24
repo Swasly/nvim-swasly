31a3c31383d9afc1c5eaf3b47e2a583960acf54a
Wesly Tonks <wesly.tonks@siemens.com>
2025-02-24 07:26:07 -0500

    change to tokyo-night to fix poorly highlighted comments

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,3 +1,16 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function() vim.cmd.colorscheme "catppuccin-mocha" end },
+  --{
+  --"catppuccin/nvim",
+  --name = "catppuccin",
+  --priority = 1000,
+  --config = function()
+  --vim.cmd.colorscheme "catppuccin-mocha"
+  --end
+  --},
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
 }
